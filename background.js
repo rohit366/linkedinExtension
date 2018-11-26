818f0874054921340039540a9741938fb3ce3dfe
Luke Avedon <lavedon@gmail.com>
2018-11-26 13:23:59 -0500

    First commit.  Icon changes when on LinkedIn.

---
+++ background.js
@@ -0,0 +1,17 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        console.log("Check if on LinkedIn.com");
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { urlContains: 'linkedin' },
+                    })
+                ],
+                // And shows the extension's page action.
+                actions: [ new chrome.declarativeContent.ShowPageAction() ]
+            }
+        ]);
+    });
+});
+
---
+++ content.js
@@ -0,0 +1,6 @@
+console.log("Mr Muffin Ready?");
+
+let paragraphs = document.getElementsByTagName('p');
+for (elt of paragraphs) {
+  elt.style['background-color'] = '#FF00FF';
+}
